4981e756a1e037b697ed4380fac1d0d4ea859a72
Alexandru Ene <alex.ene0x11@gmail.com>
2024-01-07 15:42:00 +0000

    lambda with axum and router

---
+++ Readme.md
@@ -0,0 +1,6 @@
+### Rust React Tailwind
+
+Starter project for rust react and tailwind SAAS creation.
+
+Rust is used for the backend and runs in a lambda.
+React + Tailwind on the client.
---
+++ server/src/main.rs
@@ -0,0 +1,38 @@
+//! This is an example function that leverages the Lambda Rust runtime's HTTP support
+//! and the [axum](https://docs.rs/axum/latest/axum/index.html) web framework.  The
+//! runtime HTTP support is backed by the [tower::Service](https://docs.rs/tower-service/0.3.2/tower_service/trait.Service.html)
+//! trait.  Axum applications are also backed by the `tower::Service` trait.  That means
+//! that it is fairly easy to build an Axum application and pass the resulting `Service`
+//! implementation to the Lambda runtime to run as a Lambda function.  By using Axum instead
+//! of a basic `tower::Service` you get web framework niceties like routing, request component
+//! extraction, validation, etc.
+
+use axum::{extract::Path, response::Json, routing::get, Router};
+use lambda_http::{run, Error};
+use serde_json::{json, Value};
+use tower_http::services::{ServeDir, ServeFile};
+
+async fn get_foo_name(Path(name): Path<String>) -> Json<Value> {
+    Json(json!({ "msg": format!("I am POST /foo/:name, name={name}") }))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // required to enable CloudWatch error logging by the runtime
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+
+    let serve_dir = ServeDir::new("../client/dist")
+        .not_found_service(ServeFile::new("../client/dist/index.html"));
+
+    let app = Router::new()
+        .route("/api/:name", get(get_foo_name))
+        .fallback_service(serve_dir);
+
+    run(app).await
+}
